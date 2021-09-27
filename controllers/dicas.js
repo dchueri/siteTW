ea2e091297d227a71dcd9d0028ff76a1abfe0107
diegoCasa <diegochueri@gmail.com>
2021-09-27 19:51:11 -0400

    CSS

--- controllers/dicas.js
+++
@@ -1,8 +0,0 @@
-module.exports = app => {
-    app.get('/dicas', (req, res) => res.send('Rodando ok'))
-
-    app.post('/dicas', (req, res) => {
-        console.log(req.body)    
-        res.send('Post da rota dicas')
-    })
-}
